02a87f0b4b8b7058674a9abc8ee9dc85d706d433
Brittany Storoz <brittanystoroz@gmail.com>
2020-01-13 12:33:19 -0700

    Update details

--- src/CreatureDetails.js
+++ src/CreatureDetails.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './image-display.css';
 import { Link } from 'react-router-dom';
 
-const CreatureDetails = ({ id, name, image, bio, type }) => {
+const CreatureDetails = ({ data, id }) => {
+  const { name, image, bio, type } = data.find(creature => creature.id === parseInt(id));
+
   return (
     <div>
       <Link to={`/${type}`} className='back-btn'>◀ back</Link>
       <h1>{name}</h1>
-      <img src={image} className='app-img-no-hover'/>
+      <img src={image} alt='' className='app-img-no-hover' />
       <p className='creature-bio'>{bio}</p>
     </div>
   )
