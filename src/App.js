6c9263632d20ecec2ffcf46c1083140428ca1bee
Brittany Storoz <brittanystoroz@gmail.com>
2020-01-13 12:21:34 -0700

    Add components and data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useRouteMatch, NavLink } from 'react-router-dom'
+import Home from './Home'
+import Creatures from './Creatures'
+import CreatureDetails from './CreatureDetails'
+import unicorns from './data/unicorn-data'
+import puppies from './data/puppy-data'
+import sharks from './data/shark-data'
 import './App.css';
 
-function App() {
+const App = () => {
+  const data = { unicorns, puppies, sharks };
+  const home = useRouteMatch({ path: '/', exact: true });
+  const creatures = useRouteMatch({ path: '/:creature', exact: true })
+  const creature = useRouteMatch({ path: '/:creature/:id' })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="App">
+      <header>
+        <NavLink to='/unicorns' className='nav'> Unicorns </NavLink>
+        <NavLink to='/puppies' className='nav'> Puppies </NavLink>
+        <NavLink to='/sharks' className='nav'> Sharks </NavLink>
       </header>
-    </div>
+      {home && <Home />}
+      {creatures && <Creatures data={data[creatures.params.creature]} />}
+      {creature && <CreatureDetails
+        data={data[creature.params.creature]}
+        id={creature.params.id}
+      />
+      }
+    </main>
   );
 }
 
